27864ab841945f28d2ef1aa588893c272d295353
jamesonbass1987 <jamesonbass@gmail.com>
2018-04-07 13:30:14 -0700

    updated routes and .env to gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+      namespace :v1 do
+        resources :dogs
+        resources :users
+        resources :parks, only: [:show, :index]
+        resources :ages, only: [:index]
+        resources :neighborhoods, only: [:index]
+        resources :sizes, only: [:index]
+        resources :breeds, only: [:index]
+        get 'authed_user', to: 'authentication#authed_user'
+        post 'authenticate', to: 'authentication#authenticate'
+      end
+  end
+
 end
